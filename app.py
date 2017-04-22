57165a2b2f35c050edbc2cc42163a973e5b3e05b
tiboded <tiboded@hotmail.fr>
2017-04-22 21:08:51 +0800

    usual

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-import os
-from flask import Flask
+from flask import Flask,render_template,redirect,request,url_for
+from flask_sqlalchemy import SQLAlchemy
 
+#APP INSTANCE
 app=Flask(__name__)
+app.config['DEBUG']=True
 
-@app.route('/')
-def hello():
-    return 'Hello, World !'
+#DATABASE
+SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}'.format(
+        username='webrobbie',
+        password='passworddb',
+        hostname='webrobbie.mysql.pythonanywhere-services.com',
+        databasename='webrobbie$tutodb',
+        )
+app.config['SQLALCHEMY_DATABASE_URI']=SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_POOL_RECYCLE']=299
+db=SQLAlchemy(app)
+#on PythonAnywhere :
+#from app import db
+#db.create_all()
 
-if __name__=='__main__':
-    app.debug=True
-    port=int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0',port=port)
+#MODEL
+class Comment(db.Model):
+    __tablename__='comments'
+    id=db.Column(db.Integer,primary_key=True)
+    content=db.Column(db.String(128))
+
+#ROUTE
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method=='GET':
+        return render_template('index.html',comments=Comment.query.all())
+    comment=Comment(content=request.form['content'])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
